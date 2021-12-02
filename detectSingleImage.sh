f554e390fe2a8db6ccc58b4ae41626c47c2d399b
kberg18 <kberg18@student.sdu.dk>
2021-12-02 16:37:12 +0100

    fixed relative paths in detectSingleImage

--- detectSingleImage.sh
+++ detectSingleImage.sh
@@ -1,5 +1,6 @@
 cd darknet
 
-rm /data/coco.names
-cp -r /data/$1.names /data/coco.names
+rm data/coco.names
+cp -r data/$1.names data/coco.names
+
 ./darknet detect cfg/$1.cfg $1.weights ../$2 -thresh $3
