deee20fd96c4f0eb74beef85f227cda32c8eea11
Kasper Bergstedt <kberg18@student.sdu.dk>
2021-12-07 15:34:06 +0100

    succesfully connection from rosbridge to webserver

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
-## Projekt for UAV
+## Building the image
+First run the following to build the rosbridge image
+    
+    docker build -t rosbridge .
+
+## Running the image
+Then run the following to run the container
+
+    docker run -p 9090:9090 rosbridge
--- main.py
+++ main.py
@@ -1,18 +1,43 @@
+import time
+import roslibpy
+import base64
 import cv2
-vidcap = cv2.VideoCapture("videos/Mapping.mp4")
+import logging
+
+time.sleep(5)
+
+# Configure logging to high verbosity (DEBUG)
+fmt = '%(asctime)s %(levelname)8s: %(message)s'
+logging.basicConfig(format=fmt, level=logging.DEBUG)
+log = logging.getLogger(__name__)
+
+log.info("Starting camera")
+
+client = roslibpy.Ros(host='localhost', port=9090)
+client.run()
+
+talker = roslibpy.Topic(client, '/chatter', 'std_msgs/String')
+
+vidcap = cv2.VideoCapture("/videos/Facade.mp4")
 # vidcap = cv2.VideoCapture("videos/Facade.mp4")
-bool, frame = vidcap.read()
 i = 0
-frame_count = 0
 frame_skip = 25
-output_loc = "frames/"
-while True:
+
+while client.is_connected:
+    # time.sleep(1)
+
     bool, frame = vidcap.read()
-    if not bool:
-        break
-    if i > frame_skip - 1:
-        frame_count += 1
-        cv2.imwrite(output_loc + "Frame%d.jpg" % frame_count, frame)
-        i = 0
-        continue
-    i += 1
+    log.info(bool)
+    if bool:
+        # converts the image to base64
+        bool, buffer = cv2.imencode('.jpg', frame)
+        encoded_img = base64.b64encode(buffer).decode('ascii')
+
+        talker.publish(roslibpy.Message({'data': encoded_img}))
+        
+        i += frame_skip
+        vidcap.set(cv2.CAP_PROP_POS_FRAMES, i)
+
+talker.unadvertise()
+client.terminate()
+log.info("Stopping camera")
