6e569873f4a5944cae8e7dbe5ed4af0c43ba1fbb
Michael <clonex_kontakt@hotmail.com>
2021-12-11 10:48:56 +0100

    Removed logging from main.py

--- main.py
+++ main.py
@@ -2,16 +2,8 @@ import time
 import roslibpy
 import base64
 import cv2
-import logging
 
 time.sleep(5)
-
-# Configure logging to high verbosity (DEBUG)
-fmt = '%(asctime)s %(levelname)8s: %(message)s'
-logging.basicConfig(format=fmt, level=logging.DEBUG)
-log = logging.getLogger(__name__)
-
-log.info("Starting camera")
 
 client = roslibpy.Ros(host='localhost', port=9090)
 client.run()
@@ -24,10 +16,9 @@ i = 0
 frame_skip = 25
 
 while client.is_connected:
-    # time.sleep(1)
+    time.sleep(1)
 
     bool, frame = vidcap.read()
-    log.info(bool)
     if bool:
         # converts the image to base64
         bool, buffer = cv2.imencode('.jpg', frame)
@@ -40,4 +31,3 @@ while client.is_connected:
 
 talker.unadvertise()
 client.terminate()
-log.info("Stopping camera")
