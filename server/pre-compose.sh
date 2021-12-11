465180ec954e43d0a58565cb8089e744fb53a541
Michael <clonex_kontakt@hotmail.com>
2021-12-11 13:16:20 +0100

    Changed project structure

---
+++ server/pre-compose.sh
@@ -0,0 +1,5 @@
+cd weights
+wget https://pjreddie.com/media/files/yolov3.weights --no-check-certificate
+wget https://clonex.dk/dior.weights --no-check-certificate
+wget https://clonex.dk/visdrone.weights --no-check-certificate
+wget https://clonex.dk/608.weights --no-check-certificate
---
+++ server/src/installDarknet.sh
@@ -0,0 +1,13 @@
+rm -rf darknet
+
+git clone https://github.com/pjreddie/darknet
+cd darknet
+make
+
+# wget https://pjreddie.com/media/files/yolov3.weights
+# wget https://clonex.dk/dior.weights
+# wget https://clonex.dk/visdrone.weights
+# wget https://clonex.dk/608.weights
+
+# cp -a ../cfg/ cfg/
+# cp -a ../names/ data/
